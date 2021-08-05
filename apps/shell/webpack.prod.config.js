363b3c12c382eb41dd1671369caff4b60ce15fa9
Douglas Coble <dcoble@relias.com>
2021-08-04 21:37:50 -0400

    set up new config for webpack prod

--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -1,5 +1,41 @@
-module.exports = require('./webpack.config');
+// module.exports = require('./webpack.config');
 
-module.exports.plugins[0].remotes = {
-  mfe1: 'mfe1@https://ng-mod-fed-mfe1/remoteEntry.js',
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const mf = require('@angular-architects/module-federation/webpack');
+const path = require('path');
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, '../../tsconfig.base.json'), [
+  /* mapped paths to share */
+]);
+
+module.exports = {
+  output: {
+    uniqueName: 'shell',
+    publicPath: 'auto',
+  },
+  optimization: {
+    runtimeChunk: false,
+    minimize: false,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      remotes: {
+        mfe1: 'mfe1@https://ng-mod-fed-mfe1/remoteEntry.js',
+      },
+      shared: {
+        '@angular/core': { singleton: true, strictVersion: true },
+        '@angular/common': { singleton: true, strictVersion: true },
+        '@angular/common/http': { singleton: true, strictVersion: true },
+        '@angular/router': { singleton: true, strictVersion: true },
+        ...sharedMappings.getDescriptors(),
+      },
+    }),
+    sharedMappings.getPlugin(),
+  ],
 };
